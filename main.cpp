a2a602f2d1ab6f0a4062c010ad2589965f0efeb5
Alexandr Vazhenin <aw-azs@yandex.ru>
2021-12-13 22:16:01 +0500

    add Enter A and B

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int A_var, B_var = 0;
+
+int main()
+{
+    cout << "Enter A" << endl;
+    cin >> A_var;
+
+    cout << "Enter B" << endl;
+    cin >> B_var;
+
+    return 0;
+}
