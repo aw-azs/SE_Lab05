820420984f792a13db406a7d0f7ffc8b4910ad75
Alexandr Vazhenin <aw-azs@yandex.ru>
2021-12-14 20:08:52 +0500

    add a*b out

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main()
     cout << "Enter B" << endl;
     cin >> B_var;
 
-    cout << A_var - B_var << endl;
+    cout << A_var - B_var << '\n'  << endl;
+    cout << A_var * B_var << endl;
 
     return 0;
 }
